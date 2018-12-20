b1bee6ba226178ea55be6e191a32546ef044e9cc
I. Dan Calinescu <idancalinescu@gmail.com>
2018-12-19 20:20:53 -0500

    fixed template issue

--- templates/personal/index.js
+++ templates/personal/index.js
@@ -2,8 +2,8 @@ module.exports = (fixture) => {
   const template = Object.assign({}, fixture)
 
   template.id = "personal"
-  template.fixture: "starter"
-  template.tags: ["personal", "branding", "starter"]
+  template.fixture = "starter"
+  template.tags = ["personal", "branding", "starter"]
   template.chunks.intro.routes.main.cover = "This is my personal website"
   template.chunks.intro.routes.main.cover.subtitle = "I built it myself"
   template.assets["cover.r.png"] = "https://imgur.com/download/4rUQrOC"
