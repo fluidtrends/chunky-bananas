621e7544eb1c30b7fb7106d37c7a056bf5ca2b15
I. Dan Calinescu <idancalinescu@gmail.com>
2018-12-20 01:27:49 -0500

    added smarter template structure

--- templates/personal/index.js
+++ templates/personal/index.js
@@ -1,12 +1,11 @@
-module.exports = (fixture) => {
-  const template = Object.assign({}, fixture)
-
-  template.id = "personal"
-  template.fixture = "starter"
-  template.tags = ["personal", "branding", "starter"]
-  template.chunks.intro.routes.main.cover = "This is my personal website"
-  template.chunks.intro.routes.main.cover.subtitle = "I built it myself"
-  template.assets["cover.r.png"] = "https://imgur.com/download/4rUQrOC"
-
-  return template
-}
+module.exports = () => ({
+  id: "personal",
+  fixture: "starter",
+  tags: ["personal", "branding", "starter"],
+  data: (fixture) => {
+    fixture.chunks.intro.routes.main.cover = "This is my personal website"
+    fixture.chunks.intro.routes.main.cover.subtitle = "I built it myself"
+    fixture.assets["cover.r.png"] = "https://imgur.com/download/4rUQrOC"
+    return fixture
+  }
+})
